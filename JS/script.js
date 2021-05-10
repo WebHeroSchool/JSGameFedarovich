0323281dea0dd198fe22e6e297fe09bea4b64b21
Alexey Fedarovich <alexeycryptowhale@gmail.com>
2021-05-10 19:15:59 +0300

    addObjects

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,27 @@
 let hello;
 hello = 'Привет!';
 console.log(hello);
+
+let question1 {
+	question = 'Кто создал JS', 
+	answer = '',
+	currectAnswer = 'Брэндон Эйх'
+}
+
+let question1 {
+	question = 'Сколько дней ушло на создание JS', 
+	answer = '',
+	currectAnswer = 10
+}
+
+let question1 {
+	question = 'Сколько типов данных в JS', 
+	answer = '',
+	currectAnswer = 8
+}
+
+let question1 {
+	question = 'Год создания JS', 
+	answer = '',
+	currectAnswer = 1995
+}
