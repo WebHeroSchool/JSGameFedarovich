dd9a90031e8c20829119db1e011a15a0476ff1ad
Alexey Fedarovich <alexeycryptowhale@gmail.com>
2021-05-11 10:06:16 +0300

    Правки в объектах, которые не заметил ментор...

--- JS/script.js
+++ JS/script.js
@@ -2,26 +2,26 @@ let hello;
 hello = 'Привет!';
 console.log(hello);
 
-let question1 {
-	question = 'Кто создал JS', 
-	answer = '',
-	currectAnswer = 'Брэндон Эйх'
-}
+let question1 = {
+	question: 'Кто создал JS', 
+	answer: '',
+	currectAnswer: 'Брэндон Эйх'
+};
 
-let question2 {
-	question = 'Сколько дней ушло на создание JS', 
-	answer = '',
-	currectAnswer = 10
-}
+let question2 = {
+	question: 'Сколько дней ушло на создание JS', 
+	answer: '',
+	currectAnswer: 10
+};
 
-let question3 {
-	question = 'Сколько типов данных в JS', 
-	answer = '',
-	currectAnswer = 8
-}
+let question3 = {
+	question: 'Сколько типов данных в JS', 
+	answer: '',
+	currectAnswer: 8
+};
 
-let question4 {
-	question = 'Год создания JS', 
-	answer = '',
-	currectAnswer = 1995
-}
+let question4 = {
+	question: 'Год создания JS', 
+	answer: '',
+	currectAnswer: 1995
+};
